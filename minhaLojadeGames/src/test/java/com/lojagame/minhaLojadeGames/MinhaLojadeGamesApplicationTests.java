62b7024443103972b83da94cc3b98c0f328c23bd
Isabel-Oyama <emiko.oyama93@gmail.com>
2021-01-27 16:38:35 -0300

    Atividade Loja Game

---
+++ minhaLojadeGames/src/test/java/com/lojagame/minhaLojadeGames/MinhaLojadeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojagame.minhaLojadeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojadeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
